a81d5b64360b511ac5e5f9a95e4f797bd3ca40a4
Nakagawa <yslius81@gmail.com>
2022-04-22 17:27:08 +0900

    useSWR

--- next_app/components/layout.js
+++ next_app/components/layout.js
@@ -10,7 +10,7 @@ export default function Layout(props) {
         <title>{props.title}</title>
         <link
           rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          href="/css/bootstrap.min.css"
           crossorigin="anonymous"
         ></link>
       </Head>
--- next_app/pages/__index.js
+++ next_app/pages/__index.js
@@ -1,44 +1,38 @@
-import { useState } from "react";
 import Layout from "../components/layout";
 import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Home() {
-  const url = "http://localhost:3001/_api/employees";
-  // const url = "./data.json";
-  // const [data, setData] = useState({ message: "", employees: [] });
-  // fetch(url)
-  //   .then((res) => res.json())
-  //   .then((res) => setData(res));
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR("/data.json", fetcher);
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">
-            {" "}
-            {data != undefined ? data.message : "error..."}{" "}
+            {data != undefined ? data.message : "error..."}
           </h5>
-          <table className="table bg-white">
-            <thead className="table-dark">
+          <table className="table table-dark">
+            <thead className="">
               <tr>
-                <th> Name </th> <th> Mail </th> <th> Age </th>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
               </tr>
             </thead>
             <tbody>
               {data != undefined ? (
-                data.employees.map((value, key) => (
+                data.data.map((value, key) => (
                   <tr key={key}>
-                    <td>{value.name}</td>
-                    <td>{value.sex}</td>
-                    <td></td>
+                    <th>{value.name}</th>
+                    <td>{value.mail}</td>
+                    <td>{value.age}</td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td></td>
+                  <th></th>
                   <td>no data.</td>
                   <td></td>
                 </tr>
